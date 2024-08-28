59b234e86796aa0d62fced94d563d6788abd1682
Zaid Sheikh <zsheikh@cs.cmu.edu>
2024-08-27 23:46:47 -0400

    Initial commit for TurkingBench

---
+++ datasets/turkingbench/api.py
@@ -0,0 +1,51 @@
+def modify_text(self, input_name: str, input_value) -> None:
+    """For a given input element, enter the input value.
+
+    Args:
+    ----
+        input_name (str): The name of the input element.
+        input_value: The value to enter into the input element.
+    """
+    pass
+
+
+def modify_checkbox(self, input_name: str, input_value) -> None:
+    """For a given checkbox element, check or uncheck the box.
+
+    Args:
+    ----
+        input_name (str): The name of the checkbox element.
+        input_value: The value to set the checkbox to.
+    """
+    pass
+
+
+def modify_radio(self, input_name: str, input_value) -> None:
+    """For a given radio element, select the radio button.
+
+    Args:
+    ----
+        input_name (str): The name of the radio element.
+        input_value: The value to set the radio button to.
+    """
+    pass
+
+def modify_select(self, input_name: str, input_value) -> None:
+    """For a given select element, select the option.
+
+    Args:
+    ----
+        input_name (str): The name of the select element.
+        input_value: The value to set the select element to.
+    """
+    pass
+
+def modify_range(self, input_name: str, input_value) -> None:
+    """For a given range element, set the value.
+
+    Args:
+    ----
+        input_name (str): The name of the range element.
+        input_value: The value to set the range element to.
+    """
+    pass
---
+++ datasets/turkingbench/schema_raw.py
@@ -0,0 +1,14 @@
+from typing import List, Optional
+from pydantic import BaseModel, Field, root_validator
+
+
+class SchemaRaw(BaseModel):
+    _id: str
+    Task: str
+    Title: str
+    Description: str
+    Keywords: str
+    Template: str
+    Answer: dict[str, str]
+    class Config:
+        extra = "allow"
