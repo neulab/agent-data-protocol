e4a2629d0a122fdcbabbb2804727ead5484741ab
Ketan <ramanetiketan.work@gmail.com>
2024-09-05 13:56:56 -0400

    added path to schema for imports

--- datasets/turkingbench/raw_to_standardized.py
+++ datasets/turkingbench/raw_to_standardized.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import json
+sys.path.insert(1, './')
+
 from schema.action.api import ApiAction
 from schema.observation.web import WebObservation
 from schema.trajectory import Trajectory
