55fe48e540509db166debee3117f19d7b75987b2
yueqis <yueqis@andrew.cmu.edu>
2025-05-12 22:19:10 -0400

    update system prompt to fix mock function calling

--- scripts/browsing_prompts/system.py
+++
@@ -1,9 +0,0 @@
-import os
-
-_script_dir = os.path.dirname(os.path.realpath(__file__))
-
-prompt_file = os.path.join(_script_dir, 'system_prompt.txt')
-def get_system_message():
-    with open(prompt_file, 'r') as f:
-        system_prompt = f.read()
-    return system_prompt
