2ff12a9297daa5da89ae03ed14e2e2a4e5377915
yueqis <yueqis@andrew.cmu.edu>
2025-07-08 04:06:39 -0400

    samples

--- script.sh
+++ script.sh
@@ -16,7 +16,7 @@ export PYTHONPATH=`pwd`:$PYTHONPATH
 #cat /project/flame/yueqis/agent-data-collection/datasets/$MY_DATASET/full_std.jsonl | python scripts/generate_thoughts_std.py
 
 echo Step_3
-#cat datasets/$MY_DATASET/full_std.jsonl | python -u scripts/std_to_sft.py --is_web=yes --chunk=all --api_env=browser
+cat datasets/$MY_DATASET/full_std.jsonl | python -u scripts/std_to_sft.py --is_web=yes --chunk=all --api_env=browser
 
 echo Sampling
 python add_sample.py
